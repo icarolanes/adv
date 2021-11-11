99ef91db1c2be11a4c188906de55bd7b2b303564
icaro felipe de lanes <79595101+icarolanes@users.noreply.github.com>
2021-11-10 22:49:30 -0300

    cadastro de usuario

--- sign-in/novo_acesso.php
+++ sign-in/novo_acesso.php
@@ -1,9 +1,10 @@
 <?php
+session_start();
+require_once('../banco/conexao.php');
 function nova_pessoa($cpf, $nome, $sobrenome, $email, $con)
 {
     $query = "INSERT INTO `tab_pessoas`
-    (`pessoa_id`,
-    `pessoa_nome`,
+    (`pessoa_nome`,
     `pessoa_sobrenome`,
     `pessoa_email`,
     `pessoa_cpf`)
@@ -12,6 +13,7 @@ function nova_pessoa($cpf, $nome, $sobrenome, $email, $con)
     '{$sobrenome}',
     '{$email}',
     '{$cpf}')";
+
     $resultado = $con->prepare($query);
     $resultado->execute();
 
@@ -21,16 +23,17 @@ function nova_pessoa($cpf, $nome, $sobrenome, $email, $con)
     }else{
         return(false);
     }
-};
+}
 
 function chama_pessoa($cpf,$con){
-    $query = "SELECT `pessoa_id` from tab_pessoas where pessoa_cpf = '{$cpf}'";
+   $query = "SELECT `pessoa_id` as id from tab_pessoas where pessoa_cpf = '{$cpf}'";
     $resultado = $con->prepare($query);
     $resultado->execute();
 
     $contar = $resultado->RowCount();
     if($contar>0){
-        $id = $resultado->fetch();
+        $res = $resultado->fetch();
+        $id = $res['id'];
         return($id);
     }else{
         return(stop());
@@ -45,7 +48,7 @@ function cad_usuario($cpf, $pessoa, $senha, $con)
      `usuario_cpf`,
      `usuario_senha`)
      VALUES
-     ('{$pessoa}','{$cpf}','{$senha}')";
+     ('{$pessoa}','{$cpf}','{$senha_nova}')";
      $resultado = $con->prepare($query);
      $resultado->execute();
 
@@ -62,7 +65,8 @@ function novo_usuario($cpf, $nome, $sobrenome, $email, $senha, $con)
 {
     nova_pessoa($cpf, $nome, $sobrenome, $email, $con);
     $id_pessoa = chama_pessoa($cpf, $con);
-    cad_usuario($cpf, $id_pessoa, $senha, $con);
+   $ret_us = cad_usuario($cpf, $id_pessoa, $senha, $con);
+    return($ret_us);
 }
 
 
@@ -72,3 +76,17 @@ $nome =      $_POST['nome'];
 $sobrenome = $_POST['sobrenome'];
 $email =     $_POST['email'];
 $senha =     $_POST['senha'];
+
+$no = novo_usuario($cpf, $nome, $sobrenome, $email, $senha, $con);
+
+if($no == true){
+$_SESSION['loginErro'] = ('<div class="alert alert-success" role="alert">
+ <strong>  Usuario cadastrado com sucesso</strong><br>
+</div>');
+    header("location: logar");
+}else{
+$_SESSION['loginErro'] = ('<div class="alert alert-warning" role="alert">
+ <strong>  Usuario não indentificado!</strong><br> Acesse com seu Usuario
+</div>');
+    header("location: novo_user");
+}
